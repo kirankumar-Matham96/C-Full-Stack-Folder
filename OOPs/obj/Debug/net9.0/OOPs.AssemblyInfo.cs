67c4aa440dce62b71039301f036cf522039c5c0e
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2025-11-11 16:14:12 +0530

    add: single level inheritance

--- OOPs/obj/Debug/net9.0/OOPs.AssemblyInfo.cs
+++ OOPs/obj/Debug/net9.0/OOPs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef032ffcbf1b012fc9891b53b682ba8e257115c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6e04cfc881d99ee2eb19d4cdf4dddc63cbd656")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
