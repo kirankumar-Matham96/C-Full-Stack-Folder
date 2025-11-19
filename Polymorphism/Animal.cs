e8848b905d7ff310c79f892b3db6f7c85d26dfa1
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2025-11-19 21:57:43 +0530

    update: renamed the polymorphism

---
+++ Polymorphism/Animal.cs
@@ -0,0 +1,35 @@
+namespace Polymorphism
+{
+    /** 
+     * Interface representing an animal that can make a sound.
+     * example for method overriding (runtime, dynamic, late binding) 
+     */
+    interface IAnimal
+    {
+        void MakeSound();
+    }
+
+    class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            System.Console.WriteLine("Mew!");
+        }
+    }
+
+    class Dog : IAnimal
+    {
+        public void MakeSound()
+        {
+            System.Console.WriteLine("Woof!");
+        }
+    }
+
+    class Cow : IAnimal
+    {
+        public void MakeSound()
+        {
+            System.Console.WriteLine("Mooo!");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Polymorphism
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("\n##### Welcome to Polymorphism #####\n");
+
+            Addition add = new Addition();
+
+            System.Console.WriteLine(add.add(5));
+            System.Console.WriteLine(add.add(5, 10));
+            System.Console.WriteLine(add.add(5, 10, 15));
+            System.Console.WriteLine(add.add("Hello ", "Raju"));
+
+            Cat cat = new Cat();
+            cat.MakeSound();
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+
+            Cow cow = new Cow();
+            cow.MakeSound();
+        }
+    }
+}
