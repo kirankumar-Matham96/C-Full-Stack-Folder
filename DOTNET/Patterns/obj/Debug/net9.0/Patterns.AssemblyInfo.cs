d39cf747aaf80d38d528d827b5e7259ef49b984d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2025-11-23 00:48:56 +0530

    feat: animations added in css

--- DOTNET/Patterns/obj/Debug/net9.0/Patterns.AssemblyInfo.cs
+++ DOTNET/Patterns/obj/Debug/net9.0/Patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d8f740ac8e159b55bb8f9045be68126db5bddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c49567ab12fbab780404a342f47c4529ab3410")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
